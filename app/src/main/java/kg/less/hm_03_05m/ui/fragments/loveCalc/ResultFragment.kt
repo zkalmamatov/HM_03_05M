93c0b5826f71ca476006957a339aa4b24f3e8f9a
zkalmamatov <jolboldu39@gmail.com>
2024-09-14 19:26:45 +0600

    HM_03_05M_update1

--- app/src/main/java/kg/less/hm_03_05m/ui/fragments/loveCalc/ResultFragment.kt
+++ app/src/main/java/kg/less/hm_03_05m/ui/fragments/loveCalc/ResultFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResultListener
+import androidx.navigation.fragment.findNavController
 import kg.less.hm_03_05m.R
 import kg.less.hm_03_05m.databinding.FragmentResultBinding
+import kg.less.hm_03_05m.model.LoveModel
 
 class ResultFragment : Fragment() {
 
@@ -15,25 +18,30 @@ class ResultFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentResultBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        fillInTheData()
+        initListener()
+    }
 
-        arguments?.let {
-            val firstName = it.getString("result") ?: "Unknown"
-            val secondName = it.getString("percentage") ?: "0%"
-
-            binding.fname.text = "You: $firstName"
-            binding.sname.text = "Me: $secondName"
-            binding.result.text = secondName
+    private fun fillInTheData() = with(binding) {
+        setFragmentResultListener("key") {_, bundle ->
+            val result = bundle.getSerializable("data") as? LoveModel
+            fname.text = result?.firstName
+            sname.text = result?.secondName
+            score.text = result?.percentage
+            tvResult.text = result?.result
         }
+    }
 
+    private fun initListener() {
         binding.btnAgain.setOnClickListener {
-            requireActivity().supportFragmentManager.popBackStack()
+            findNavController().navigate(R.id.loveCalcFragment)
         }
     }
 }
